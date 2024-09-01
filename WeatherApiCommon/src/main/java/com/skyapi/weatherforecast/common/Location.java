a242afe31fc694ccb6ff117d29d20154d57998f6
ductutran542@gmail.com <ductutran542@gmail.com>
2024-09-01 17:52:54 +0700

    Fix

---
+++ WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
@@ -0,0 +1,98 @@
+package com.skyapi.weatherforecast.common;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Locations")
+public class Location {
+
+	@Column(length = 12, nullable = false, unique = true)
+	@Id
+	private String code;
+	
+	@Column(length = 128, nullable = false)
+	@JsonProperty("city_name")
+	private String cityName;
+	
+	@Column(length = 128)
+	@JsonProperty("region_name")
+	private String regionName;
+	
+	@Column(length = 64, nullable = false)
+	@JsonProperty("country_name")
+	private String countryName;
+	
+	@Column(length = 2, nullable = false)
+	@JsonProperty("country_code")
+	private String countryCode;
+	
+	private boolean enabled;
+	
+	//for delete method
+	@JsonIgnore
+	private boolean trashed;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public String getRegionName() {
+		return regionName;
+	}
+
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
+
+	public String getCountryName() {
+		return countryName;
+	}
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public boolean isTrashed() {
+		return trashed;
+	}
+
+	public void setTrashed(boolean trashed) {
+		this.trashed = trashed;
+	}
+	
+	
+}
