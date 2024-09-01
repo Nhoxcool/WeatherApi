251c7e58763c6b596ae6a5eb397ff97b97e4cd59
ductutran542@gmail.com <ductutran542@gmail.com>
2024-09-01 19:15:35 +0700

    LocationControllerTesting

--- WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
+++ WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
@@ -1,5 +1,7 @@
 package com.skyapi.weatherforecast.common;
 
+import java.util.Objects;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -7,6 +9,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "Locations")
@@ -14,22 +18,27 @@ public class Location {
 
 	@Column(length = 12, nullable = false, unique = true)
 	@Id
+	@NotBlank
 	private String code;
 	
 	@Column(length = 128, nullable = false)
 	@JsonProperty("city_name")
+	@NotBlank
 	private String cityName;
 	
 	@Column(length = 128)
 	@JsonProperty("region_name")
+	@NotNull
 	private String regionName;
 	
 	@Column(length = 64, nullable = false)
 	@JsonProperty("country_name")
+	@NotBlank
 	private String countryName;
 	
 	@Column(length = 2, nullable = false)
 	@JsonProperty("country_code")
+	@NotBlank
 	private String countryCode;
 	
 	private boolean enabled;
@@ -93,6 +102,24 @@ public class Location {
 	public void setTrashed(boolean trashed) {
 		this.trashed = trashed;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(code);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		return Objects.equals(code, other.code);
+	}
+	
 	
 	
 }
--- WeatherApiService/src/test/java/com/skyapi/weatherforecast/location/LocationApiControllerTests.java
+++ WeatherApiService/src/test/java/com/skyapi/weatherforecast/location/LocationApiControllerTests.java
@@ -1,9 +1,15 @@
 package com.skyapi.weatherforecast.location;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -23,6 +29,7 @@ public class LocationApiControllerTests {
 	
 	@MockBean LocationService service;
 	
+	
 	@Test
 	public void testAddShouldReturn400BadRequest() throws Exception {
 		Location location = new Location();
@@ -32,8 +39,34 @@ public class LocationApiControllerTests {
 		mockMvc.perform(post(END_POINT_PATH)
 				.contentType("application/json")
 				.content(bodyContent))
-				.andExpect(status()
-				.isBadRequest());
+				.andExpect(status().isBadRequest())
+				.andDo(print());
 		
+	}
+	
+	@Test
+	public void testAddShouldReturn201Created() throws Exception {
+		Location location = new Location();
+		location.setCode("NYC_USA");
+		location.setCityName("New York City");
+		location.setRegionName("New York");
+		location.setCountryCode("US");
+		location.setCountryName("United States of America");
+		location.setEnabled(true);
+		
+		Mockito.when(service.add(location)).thenReturn(location);
+		
+		String bodyContent = mapper.writeValueAsString(location);
+		
+		
+		mockMvc.perform(post(END_POINT_PATH)
+				.contentType("application/json")
+				.content(bodyContent))
+				.andExpect(status().isCreated())
+				.andExpect(content().contentType("application/json"))
+				.andExpect(jsonPath("$.code", is("NYC_USA")))
+				.andExpect(jsonPath("$.city_name", is("New York City")))
+				.andExpect(header().string("Location", "/v1/locations/NYC_USA"))
+				.andDo(print());
 	}
 }
